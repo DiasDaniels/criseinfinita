658b98fde82daf9f08e36317b49203391fa8ae28
DanielDias <cdias.jasper@gmail.com>
2020-07-19 20:51:12 -0300

    Correção de Bug 100 ++ hp

--- player/main.py
+++ player/main.py
@@ -7,8 +7,11 @@ class Player(Char):
     def turnHeal(self):
         if self.hp < 100:
             self.hp += 10
+            if self.hp > 100:
+                self.hp = 100
             print(f'Você ganhou +10HP')
             print(f'Você está com ',self.hp,' de HP')
+
         else:
             print('Você está descansado, vida Máxima atingida!')
 
