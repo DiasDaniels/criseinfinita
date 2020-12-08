c3b6c6718325e93a81c63a4b00c8a35853cfa863
Yan Doro <yan_mvd@hotmail.com>
2020-12-08 15:01:17 -0300

    Corrigido sistema de XP

--- player/main.py
+++ player/main.py
@@ -1,32 +1,35 @@
-from  char import Char
+from char import Char
+
+
 class Player(Char):
     def __init__(self, name, hp, atk, arm):
-        super().__init__ (name, hp, atk, arm)
+        super().__init__(name, hp, atk, arm)
         self.__score = 0
         self.setCallableName('Você')
-    
+
     def turnHeal(self):
         if self.hp < 100:
             self.hp += 10
             if self.hp > 100:
                 self.hp = 100
             print(f'Você ganhou +10HP')
-            print(f'Você está com ',self.hp,' de HP')
+            print(f'Você está com ', self.hp, ' de HP')
 
         else:
             print('Você está descansado, vida Máxima atingida!')
 
-    def setScore(self,score:int):
-        if score in [100, 300, 600, 1200, 2400, 4800]:
+    def setScore(self, score: int):
+        if (score * 100) in [100, 300, 600, 1200, 2400, 4800]:
             print('Você subiu de nível! ATK + 10 || DEF + 5 || Vida regenerada!')
             self.atk += 10
             self.arm += 5
-            self.hp = 100
+            self.hp = 1000
+            self.__score = 100
 
         else:
             print('Você recebeu 100 de experiência!')
-            print('Você tem', self.__score + 100, 'de experiência!')
-        self.__score = score
+            self.__score = score * 100
+            print('Você tem', self.__score, 'de experiência!')
 
     def getScore(self):
         return self.__score
